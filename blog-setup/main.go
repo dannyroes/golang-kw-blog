b1ebe6ba54b6cc359966cf373d883c8289121bac
Danny R <dannyroes@users.noreply.github.com>
2018-01-07 14:20:05 -0500

    Fix DB dependency

--- blog-setup/main.go
+++ blog-setup/main.go
@@ -7,18 +7,17 @@ package main
 import (
 	"fmt"
 	"github.com/dannyroes/golangkwblog/blog"
-	"github.com/dannyroes/golangkwblog/database"
 	"gopkg.in/mgo.v2/bson"
 	"time"
 )
 
 func main() {
 	//Clear the database
-	err := database.Connect()
+	err := blog.ConnectDB()
 	if err != nil {
 		panic(err)
 	}
-	database.DB.C("posts").RemoveAll(bson.M{})
+	blog.DB.C("posts").RemoveAll(bson.M{})
 
 	//Create three posts and insert them
 	post := blog.Post{}
@@ -26,7 +25,7 @@ func main() {
 	post.Title = "Hello World!"
 	post.Body = "This is my first post on my new blog!"
 	post.PostedTime = time.Now().Add(-5 * time.Hour)
-	err = database.InsertPost(post)
+	_, err = blog.InsertPost(post)
 
 	if err != nil {
 		panic(err)
@@ -36,7 +35,7 @@ func main() {
 	post.Title = "Is this working?"
 	post.Body = "I can't seem to view my posts, something must be broken."
 	post.PostedTime = time.Now().Add(-4 * time.Hour)
-	err = database.InsertPost(post)
+	_, err = blog.InsertPost(post)
 
 	if err != nil {
 		panic(err)
@@ -46,7 +45,7 @@ func main() {
 	post.Title = "Still coding"
 	post.Body = "Still trying to fix my blog, reading lots of docs at <a href='https://golang.org/doc'>golang.org</a>"
 	post.PostedTime = time.Now().Add(-5 * time.Minute)
-	err = database.InsertPost(post)
+	_, err = blog.InsertPost(post)
 
 	if err != nil {
 		panic(err)
